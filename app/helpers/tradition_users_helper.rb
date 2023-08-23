c7db9bc0e55b534c90c0a6419a25138288f8ad07
uto8 <70425214+uto8@users.noreply.github.com>
2023-08-23 17:48:46 +0900

    tradion参加ページ追加

--- app/helpers/tradition_users_helper.rb
+++ app/helpers/tradition_users_helper.rb
@@ -1,6 +1,6 @@
 module TraditionUsersHelper
   def joined(tradition_id)
-    if TraditionUser.find_by(tradition_id:tradition_id ,user_id: current_user.id)
+    if TraditionUser.exists?(tradition_id:tradition_id ,user_id: current_user.id)
       return true
     else
       return false
